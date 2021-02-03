c6fcc8ecdc1b836eaba5e41f499e246fce4ba68b
HEnry Hernandez <itierdd@gmail.com>
2021-02-02 22:33:56 -0600

    version 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+export interface InfoPagina{
+  title?: string;
+  email?: string;
+  name_copy?: string;
+  page_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  dribble?: string;
+  equipo_trabajo?: any[];
+}
